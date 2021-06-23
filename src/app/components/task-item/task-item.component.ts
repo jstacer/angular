9324374a7d77e7129c63448e5ba2fb4a99d4c07d
Jud Stacer <jud.stacer@gmail.com>
2021-06-22 22:00:59 -0400

    tasks can delete on click of x

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -8,12 +9,16 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
 @Input() task: Task;
+@Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+faTimes = faTimes;
 
   constructor() {
     this.task = { text: 'duh', day: 'wednesday', reminder: false};
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onDelete(task:Task) {
+    this.onDeleteTask.emit(task);
+  }
 }
